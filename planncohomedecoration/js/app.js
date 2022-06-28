a274bc80ad67451d42cfbe8359c5777d75ca25a0
yellhtetaung <yairhtet248.yha@gmail.com>
2022-06-28 12:14:53 +0630

    28/6/22

---
+++ planncohomedecoration/js/app.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    // Start Header
+    // Start nav
+
+    // for bugger
+    $(".navbuttons").click(function () {
+        $(".navbuttons").toggleClass("crossxs")
+    })
+
+    // for nav
+    $(window).scroll(function () {
+        let getscrolltop = $(this).scrollTop();
+
+        if (getscrolltop >= 200) {
+            $(".navbar").addClass("navmenus");
+        } else {
+            $(".navbar").removeClass("navmenus");
+        }
+    })
+
+    // End nav
+
+    // End Header
+
+    // Start Properties
+
+    $(".propertylists").click(function () {
+        // for active item
+
+        // $(this).addClass("activeitems");
+        // $(this).siblings().removeClass("activeitems");
+
+        $(this).addClass("activeitems").siblings().removeClass("activeitems");
+
+
+        // for filter
+
+        let getattvalue = $(this).attr("data-filter");
+        // console.log(getattvalue);
+
+        if (getattvalue === "all") {
+            $(".filters").show("slide", 500);
+        } else {
+            $(".filters").not("." + getattvalue).hide("slide", 500);
+            $(".filters").filter("." + getattvalue).show("slide", 500);
+        }
+
+
+    });
+
+
+    // End Properties
+});
