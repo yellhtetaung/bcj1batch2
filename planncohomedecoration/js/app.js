b74b3601743711fd511fd6e9e2cfea03104c85ed
yellhtetaung <yairhtet248.yha@gmail.com>
2022-07-03 08:05:10 +0630

    3/7/22

--- planncohomedecoration/js/app.js
+++ planncohomedecoration/js/app.js
@@ -41,6 +41,7 @@ $(document).ready(function () {
         if (getattvalue === "all") {
             $(".filters").show("slide", 500);
         } else {
+            $(".filters").hide();
             $(".filters").not("." + getattvalue).hide("slide", 500);
             $(".filters").filter("." + getattvalue).show("slide", 500);
         }
@@ -48,6 +49,25 @@ $(document).ready(function () {
 
     });
 
+    lightbox.option({
+        showImageNumberLabel: false,
+    })
+
 
     // End Properties
+
+
+    // Start Adv Section
+    $(window).scroll(function () {
+        let getscrolltop = $(this).scrollTop();
+
+        if (getscrolltop >= 900) {
+            $(".advimages").addClass('fromlefts');
+            $(".advtexts").addClass('fromrights');
+        } else {
+            $(".advimages").removeClass('fromlefts');
+            $(".advtexts").removeClass('fromrights');
+        }
+    })
+    // End Adv Section
 });
