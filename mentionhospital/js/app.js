b3584f3b23e838278a082cb620fde02d283b929b
yellhtetaung <yairhtet248.yha@gmail.com>
2022-07-29 18:39:58 +0630

    updated

--- mentionhospital/js/app.js
+++ mentionhospital/js/app.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
-  console.log('hi')
-})
+$(document).ready(function() {
+    // console.log('i am working')
+
+    // Start Header Section
+
+    // Start Banner Section 
+
+    // $(".carousel").carousel({
+    //     interval: 1000;
+    // })
+
+    // End Banner Section 
+
+    // End Header Section
+});
