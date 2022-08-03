71f0420c15f765dc9bad87465354e7ab462673f8
yellhtetaung <yairhtet248.yha@gmail.com>
2022-08-04 04:10:56 +0630

    4/8/22

--- mentionhospital/js/app.js
+++ mentionhospital/js/app.js
@@ -1,15 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // console.log('i am working')
-
     // Start Header Section
-
-    // Start Banner Section 
-
+    // Start Banner Section
     // $(".carousel").carousel({
     //     interval: 1000;
     // })
-
-    // End Banner Section 
-
+    // End Banner Section
     // End Header Section
+
+    // Start Info Section
+
+    $(window).scroll(function () {
+        let getscrollpoint = $(this).scrollTop();
+        // console.log(getscrollpoint);
+
+        if (getscrollpoint >= 220) {
+            $(".infotexts").addClass("fromlefts");
+            $(".infopics").addClass("fromrights");
+        } else {
+            $(".infotexts").removeClass("fromlefts");
+            $(".infopics").removeClass("fromrights");
+        }
+    });
+
+    // End Info Section
 });
