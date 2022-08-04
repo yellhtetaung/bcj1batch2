e7e258e0fe6e00a13c5b5e045359b04860822130
yellhtetaung <yairhtet248.yha@gmail.com>
2022-08-04 11:02:52 +0630

    updated

--- mentionhospital/js/app.js
+++ mentionhospital/js/app.js
@@ -24,4 +24,33 @@ $(document).ready(function () {
     });
 
     // End Info Section
+
+    // Start Premises Section
+
+    $("#lightslider").lightSlider({
+        // auto: true,
+        item: 4,
+        loop: true,
+        slideMove: 3,
+        speed: 600
+    }).play();
+    // End Premises Section
+
+    // Start Pricing Section
+    $(window).scroll(function (){
+       let getscroll = $(this).scrollTop();
+       // console.log(getscroll);
+
+        if(getscroll >= 2450){
+            $(".cardones").addClass("movelefts");
+            $(".cardtwos").addClass("movebottoms");
+            $(".cardthrees").addClass("moverights");
+        } else{
+            $(".cardones").removeClass("movelefts");
+            $(".cardtwos").removeClass("movebottoms");
+            $(".cardthrees").removeClass("moverights");
+        }
+
+    });
+    // End Pricing Section
 });
